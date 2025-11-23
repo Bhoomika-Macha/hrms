6a813dab57232e16ea32645550ea17a44bd44a88
Bhoomika-Macha <bhoomikamacha03@gmail.com>
2025-11-24 00:04:43 +0530

    Updated API base URL for Netlify deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hrms-backend-7jox.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
